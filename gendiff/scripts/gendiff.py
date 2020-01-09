cf39bc52db782231a4dee1ead0f2e0c98e65ab22
karomag <roma-novikov@mail.ru>
2020-01-09 15:14:24 +0300

    starting point

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Gendiff main script."""
+
+import argparse
+
+
+def main():
+    """Starting point."""
+    print('Gendiff')
+
+
+if __name__ == '__main__':
+    main()
