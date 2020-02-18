5bb9495a0b293b36d7f00a7713d58d2acebeb5e2
karomag <roma-novikov@mail.ru>
2020-02-18 23:52:30 +0300

    Added test build diff.

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -2,7 +2,9 @@
 
 """Tests for generate_diff."""
 
-from gendiff.build_diff import generate_diff
+import json
+
+from gendiff.build_diff import build_diff, generate_diff, read_file
 
 
 def test_generate_diff_nested_structures():
@@ -25,3 +27,12 @@ def test_generate_diff_yaml():
     with open('gendiff/tests/fixtures/diff_plain_files.txt') as inf:
         correct_answer = inf.read()
     assert result_generate_diff == correct_answer
+
+
+def test_build_diff():
+    """Test build diff."""
+    before_dict = read_file('gendiff/tests/fixtures/before.json')
+    after_dict = read_file('gendiff/tests/fixtures/after.json')
+    with open('gendiff/tests/fixtures/build_diff.txt') as inf:
+        correct_answer = inf.read()
+    assert json.dumps(build_diff(before_dict, after_dict)) == correct_answer
